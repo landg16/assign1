1f0bbb9da55c966f83c0c614b28335d6721fd7a4
lasha <landg16@freeuni.edu.ge>
2019-05-12 01:58:39 +0400

    Converted project into InteliJ project Added char grid tests

--- hw1CodeCamp/assign1/CharGridTest.java
+++ hw1CodeCamp/assign1/CharGridTest.java
@@ -52,7 +52,150 @@ public class CharGridTest {
 		assertEquals(1, cg.charArea('%'));
 		assertEquals(4, cg.charArea('b'));
 	}
-	
-	// TODO Add more tests
-	
+
+	@Test
+	public void testCharArea4() {
+		char[][] grid = new char[][] {
+				{'a', ' ', ' '},
+				{' ', ' ', ' '},
+				{' ', ' ', 'a'}
+		};
+
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(9, cg.charArea('a'));
+		assertEquals(9, cg.charArea(' '));
+	}
+
+	@Test
+	public void testCharArea5() {
+		char[][] grid = new char[][] {
+				{' ', ' ', 'y', ' ', ' '}
+		};
+
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(1, cg.charArea('y'));
+		assertEquals(5, cg.charArea(' '));
+	}
+
+	@Test
+	public void testPlusFind1() {
+		char[][] grid = new char[][] {
+				{' ', ' ', 'p', ' ', ' ', ' ', ' ', ' ', ' '},
+				{' ', ' ', 'p', ' ', ' ', ' ', ' ', 'x', ' '},
+				{'p', 'p', 'p', 'p', 'p', ' ', 'x', 'x', 'x'},
+				{' ', ' ', 'p', ' ', ' ', 'y', ' ', 'x', ' '},
+				{' ', ' ', 'p', ' ', 'y', 'y', 'y', ' ', ' '},
+				{'z', 'z', 'z', 'z', 'z', 'y', 'z', 'z', 'z'},
+				{' ', ' ', 'x', 'x', ' ', 'y', ' ', ' ', ' '}
+		};
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(2, cg.countPlus());
+	}
+
+	@Test
+	public void testPlusFind2() {
+		char[][] grid = new char[][] {
+				{' ', ' ', ' ', ' ', ' ', 'p'},
+				{' ', ' ', 'p', ' ', ' ', 'p'},
+				{' ', 'p', 'p', 'p', ' ', 'p'},
+				{' ', ' ', 'p', ' ', ' ', 'p'},
+				{' ', ' ', ' ', ' ', 'p', 'p'}
+		};
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(1, cg.countPlus());
+	}
+
+	@Test
+	public void testPlusFind3() {
+		char[][] grid = new char[][] {
+				{' ', ' ', 'p', ' ', ' ', ' ', ' ', ' ', ' '},
+				{' ', ' ', 'p', ' ', ' ', ' ', 'x', 'x', 'x'},
+				{'p', 'p', 'p', 'p', 'p', ' ', 'x', 'x', 'x'},
+				{' ', ' ', 'p', 'p', ' ', 'y', 'x', 'x', 'x'},
+				{' ', ' ', 'p', ' ', 'y', 'y', 'y', ' ', ' '},
+				{'z', 'z', 'z', 'z', 'y', 'y', 'z', 'z', 'z'},
+				{' ', ' ', 'x', 'x', ' ', 'y', ' ', ' ', ' '}
+		};
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(2, cg.countPlus());
+	}
+
+	@Test
+	public void testPlusFind4() {
+		char[][] grid = new char[][] {
+				{' ', ' ', 'p', ' ', ' ', ' ', ' ', ' ', ' '},
+				{' ', ' ', 'p', 'y', ' ', ' ', 'x', 'x', 'x'},
+				{'p', 'p', 'p', 'p', 'p', ' ', 'x', 'x', 'x'},
+				{' ', ' ', 'p', 'p', ' ', 'y', 'x', 'x', 'x'},
+				{' ', ' ', 'p', ' ', 'y', 'y', 'y', ' ', ' '},
+				{'z', 'z', 'z', 'z', 'y', 'y', 'z', 'z', 'z'},
+				{' ', ' ', 'x', 'x', ' ', 'y', ' ', ' ', ' '}
+		};
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(2, cg.countPlus());
+	}
+
+	@Test
+	public void testPlusFind5() {
+		char[][] grid = new char[][] {
+				{' ', ' ', ' ', 'a', ' ', ' ', ' '},
+				{' ', 'b', ' ', 'a', ' ', 'b', ' '},
+				{' ', ' ', ' ', 'a', ' ', ' ', ' '},
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'},
+				{' ', ' ', ' ', 'a', ' ', ' ', ' '},
+				{' ', 'b', ' ', 'a', ' ', 'b', ' '},
+				{' ', ' ', ' ', 'a', ' ', ' ', ' '}
+		};
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(1, cg.countPlus());
+	}
+
+	@Test
+	public void testPlusFind6() {
+		char[][] grid = new char[][] {};
+		CharGrid cg = new CharGrid(grid);
+		assertEquals(0, cg.countPlus());
+	}
+
+	@Test
+	public void testPlusFind7() {
+		char[][] grid = new char[][] {
+				{'x', 'x', 'x', ' '},
+				{' ', 'x', ' ', ' '}
+		};
+
+		CharGrid cg = new CharGrid(grid);
+
+		assertEquals(0, cg.countPlus());
+	}
+
+	@Test
+	public void testcountPlus7() {
+		char[][] grid = new char[][] {
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'},
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'},
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'},
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'},
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'},
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'},
+				{'a', 'a', 'a', 'a', 'a', 'a', 'a'}
+		};
+
+		CharGrid cg = new CharGrid(grid);
+		assertEquals(0, cg.countPlus());
+	}
 }
